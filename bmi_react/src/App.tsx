942b25c6496c471a1ce5380cac47287b608405e0
Matheus Levi <matheuslevi545@gmail.com>
2022-09-26 16:10:13 -0300

    Vite+React+Typescript+Tailwind

---
+++ bmi_react/src/App.tsx
@@ -0,0 +1,16 @@
+import { Forms } from './components/Forms'
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Forms />
+      <h3 className=" bg-black">
+        Simple React Typescript Tailwind Sample
+      </h3>
+    </div>
+  )
+}
+
+export default App
