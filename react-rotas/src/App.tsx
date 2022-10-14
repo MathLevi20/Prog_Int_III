39d409e08d1990106f53249edd8c327b7cae291c
Matheus Levi <matheuslevi545@gmail.com>
2022-10-14 01:13:45 -0300

    feat: todo app

---
+++ react-rotas/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Routes from './components/app'
+import Home from './components/home'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  )
+}
+
+export default App
